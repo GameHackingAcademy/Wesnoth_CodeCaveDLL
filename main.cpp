a0bee0ccce186ab1180dd66c8b4b90492d829886
attilathedud <attilathedud@gmail.com>
2020-07-22 18:17:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-	A DLL that redirects the "Terrain Description" function to a custom function that sets the player's gold to 888. 
+	A DLL that redirects the "Terrain Description" function Wesnoth 1.14.9 to a custom function that sets the player's gold to 888. 
 	This custom function then recreates the "Terrain Description" function and returns execution to the program.
 	
 	This is done through the use of a codecave. When injected, the DLL modifies the function that displays the
